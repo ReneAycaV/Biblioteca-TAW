cad46e382f0936ca4653dedaa575c757c2e290e8
Cherrios-HA <chems2623@gmail.com>
2026-04-27 13:59:10 -0400

    Creacion de rama e implementacion frontend login

--- frontend/src/app/features/auth/login/login.component.ts
+++ frontend/src/app/features/auth/login/login.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  
+  // 1. Variable para saber en qué vista estamos (por defecto 'student')
+  activeRole: string = 'student'; 
 
+  loginData = {
+    email: '',
+    password: ''
+  };
+
+  // 2. Función que se activa al hacer click en los botones
+  setRole(role: string) {
+    this.activeRole = role;
+    // Opcional: limpiar los campos al cambiar de pestaña
+    this.loginData.email = '';
+    this.loginData.password = '';
+  }
+
+  onSubmit() {
+    // Ahora podemos saber qué tipo de usuario intenta entrar
+    console.log(`Intentando ingresar como ${this.activeRole} con:`, this.loginData);
+    alert(`Ingresando como: ${this.activeRole.toUpperCase()}`);
+  }
 }
