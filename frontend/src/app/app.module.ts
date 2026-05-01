1d45355cc8ea797f81608490e43452df770e9ccf
Diego Ferrada <dieferr12@gmail.com>
2026-05-01 14:56:28 -0400

    Agrega modulo de menu principal a modulo de app

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,17 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CatalogoModule } from './features/catalogo/catalogo.module';
+import { MenuPrincipalModule } from './features/menu-principal/menu-principal.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CatalogoModule
+    CatalogoModule,
+    MenuPrincipalModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
