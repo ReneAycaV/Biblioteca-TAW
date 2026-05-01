2eb94dcbd29aea4980f8bb233b7c65c343d9f86f
Diego Ferrada <dieferr12@gmail.com>
2026-05-01 15:05:28 -0400

    Agrega imports y exports a modulo de menu principal, exporta componentes a modulo routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,14 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ListaLibrosComponent } from './features/catalogo/lista-libros/lista-libros.component';
 import { DetalleLibroComponent } from './features/catalogo/detalle-libro/detalle-libro.component';
+import { MenuPrincipalUsuarioComponent } from './features/menu-principal/menu-principal-usuario/menu-principal-usuario.component';
+import { MenuPrincipalAdminComponent } from './features/menu-principal/menu-principal-admin/menu-principal-admin.component';
 
 const routes: Routes = [
-  { path: 'catalogo',     component: ListaLibrosComponent },
+  { path: 'catalogo', component: ListaLibrosComponent },
   { path: 'catalogo/:id', component: DetalleLibroComponent },
+  { path: 'menu-usuario', component: MenuPrincipalUsuarioComponent },
+  { path: 'menu-admin', component: MenuPrincipalAdminComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
