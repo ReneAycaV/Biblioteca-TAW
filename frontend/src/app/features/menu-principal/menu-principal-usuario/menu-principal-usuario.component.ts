c5f2cd93147fceda2468806cee05e9fc28ea63fd
Diego Ferrada <dieferr12@gmail.com>
2026-05-01 23:02:22 -0400

    Agrega encabezado a menu principal de usuario

--- frontend/src/app/features/menu-principal/menu-principal-usuario/menu-principal-usuario.component.ts
+++ frontend/src/app/features/menu-principal/menu-principal-usuario/menu-principal-usuario.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-menu-principal-usuario',
   templateUrl: './menu-principal-usuario.component.html',
-  styleUrls: ['./menu-principal-usuario.component.css']
+  styleUrls: ['./menu-principal-usuario.component.css'],
 })
 export class MenuPrincipalUsuarioComponent {
-
+  userName = 'Juan González';
 }
