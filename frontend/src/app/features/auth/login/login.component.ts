c511348fa000c465a0cd89df9bafacbaddafec67
Cherrios-HA <chems2623@gmail.com>
2026-05-04 18:28:04 -0400

    se agrego funcion de verificacion para ingreso como alumno y/o admin

--- frontend/src/app/features/auth/login/login.component.ts
+++ frontend/src/app/features/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,25 +11,45 @@ export class LoginComponent {
   activeRole: string = 'student'; 
 
   loginData = {
-    netid: '',
-    password: ''
+    email: '',
+    password: '',
+    role: 'student'
   };
 
-  setRole(role: string) {
-    this.activeRole = role;
-    this.loginData.netid = '';
+  setRole(roleSelected: string) {
+    this.activeRole = roleSelected;
+    this.loginData.role = roleSelected;
+    this.loginData.email = '';
     this.loginData.password = '';
   }
 
-  // MODIFICAMOS ESTA FUNCIÓN para que reciba el formulario (form: any)
+  constructor(private router: Router) { }
+
   onSubmit(form: any) {
+    console.log("ingreso con:", this.loginData.email, this.loginData.password, this.loginData.role);
     // Si el formulario es inválido (faltan datos), detenemos todo aquí
     if (form.invalid) {
       console.log("Faltan datos, no se envía nada al backend.");
       return; 
     }
+      const accesoPermitido = this.testBD(this.loginData.email, this.loginData.password, this.loginData.role);
 
-    console.log(`Intentando ingresar como ${this.activeRole} con:`, this.loginData);
-    alert(`Ingresando como: ${this.activeRole.toUpperCase()}`);
+        if (accesoPermitido){
+          this.router.navigate(['/catalogo']);
+        } else {
+          alert("Credenciales incorrectas o no tienes permiso.");
+        }
+      }
+
+    testBD(email: string, password: string, role: string): boolean {
+      const validStudent = { email: 'alumno@nyu.com', password: '123456', role: 'student' };
+      const validAdmin = { email: 'admin_01', password: 'admin123', role: 'admin' };
+
+      if (role === 'student') {
+        return email === validStudent.email && password === validStudent.password;
+      } else if (role === 'admin') {
+        return email === validAdmin.email && password === validAdmin.password;
+      }
+      return false;
+    }
   }
-}
