473c9de148eef2c065060e28a33802950d36be23
Cherrios-HA <chems2623@gmail.com>
2026-05-03 20:29:41 -0400

    Agrega validaciones, mensajes de error y estilos finales al login

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CatalogoModule } from './features/catalogo/catalogo.module';
+import { LoginComponent } from './features/auth/login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CatalogoModule
+    CatalogoModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/features/auth/login/login.component.ts
+++ frontend/src/app/features/auth/login/login.component.ts
@@ -7,24 +7,27 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
   
-  // 1. Variable para saber en qué vista estamos (por defecto 'student')
   activeRole: string = 'student'; 
 
   loginData = {
-    email: '',
+    netid: '',
     password: ''
   };
 
-  // 2. Función que se activa al hacer click en los botones
   setRole(role: string) {
     this.activeRole = role;
-    // Opcional: limpiar los campos al cambiar de pestaña
-    this.loginData.email = '';
+    this.loginData.netid = '';
     this.loginData.password = '';
   }
 
-  onSubmit() {
-    // Ahora podemos saber qué tipo de usuario intenta entrar
+  // MODIFICAMOS ESTA FUNCIÓN para que reciba el formulario (form: any)
+  onSubmit(form: any) {
+    // Si el formulario es inválido (faltan datos), detenemos todo aquí
+    if (form.invalid) {
+      console.log("Faltan datos, no se envía nada al backend.");
+      return; 
+    }
+
     console.log(`Intentando ingresar como ${this.activeRole} con:`, this.loginData);
     alert(`Ingresando como: ${this.activeRole.toUpperCase()}`);
   }
