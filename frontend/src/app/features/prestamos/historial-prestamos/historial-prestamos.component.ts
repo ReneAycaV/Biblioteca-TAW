910593678b1dd52a43332bc2a8ee09dafd93c745
Diego Ferrada <dieferr12@gmail.com>
2026-05-22 16:23:32 -0400

    Agrega titulo de página de historial

--- frontend/src/app/features/prestamos/historial-prestamos/historial-prestamos.component.ts
+++ frontend/src/app/features/prestamos/historial-prestamos/historial-prestamos.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import { Libro } from '../../catalogo/lista-libros/lista-libros.component';
+import { ListaLibrosComponent } from '../../catalogo/lista-libros/lista-libros.component';
+
+export interface Prestamo {
+  id: number;
+  tipo: 'prestamo' | 'reserva';
+  fechaRetiro: Date;
+  fechaDevolucion: Date;
+  estado: 'activo' | 'finalizado';
+}
 
 @Component({
   selector: 'app-historial-prestamos',
   templateUrl: './historial-prestamos.component.html',
-  styleUrls: ['./historial-prestamos.component.css']
+  styleUrls: ['./historial-prestamos.component.css'],
 })
 export class HistorialPrestamosComponent {
-
+  //prestamos: Prestamo[] = [];
+  prestamos: Prestamo[] = [
+    {
+      id: 1,
+      tipo: 'prestamo',
+      fechaRetiro: new Date('2024-05-01'),
+      fechaDevolucion: new Date('2024-05-15'),
+      estado: 'finalizado',
+    },
+    {
+      id: 2,
+      tipo: 'reserva',
+      fechaRetiro: new Date('2024-06-10'),
+      fechaDevolucion: new Date('2024-06-20'),
+      estado: 'activo',
+    },
+    {
+      id: 3,
+      tipo: 'prestamo',
+      fechaRetiro: new Date('2024-04-10'),
+      fechaDevolucion: new Date('2024-04-20'),
+      estado: 'finalizado',
+    },
+    {
+      id: 4,
+      tipo: 'reserva',
+      fechaRetiro: new Date('2024-07-01'),
+      fechaDevolucion: new Date('2024-07-10'),
+      estado: 'activo',
+    },
+  ];
 }
